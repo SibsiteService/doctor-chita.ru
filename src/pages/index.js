f6f6d04c4bfeec779fa6edb4d4297c0091246486
GOOSEBERRY <gooseberry.plusplus@gmail.com>
2019-10-20 21:26:14 +0300

    test yandex metrica

--- src/pages/index.js
+++ src/pages/index.js
@@ -46,6 +46,21 @@ class Index extends React.Component {
         дополнительное мнение экспертов об онкологии, ДНК анализы. 
         Ваш Доктор - официальный партнер лаборатории Гемотест в Чите. 
         У нас Вы сможете пройти весь спектр диагностики здоровья и узнать о Ваших болезнях на ранних этапах."></meta>
+        
+
+        <script type="text/javascript" >
+   (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+   m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+   ym(55704484, "init", {
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true,
+        webvisor:true
+   });
+</script>
+<noscript><div><img src="https://mc.yandex.ru/watch/55704484" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
         </Helmet>        
         <Header/>
         <Waypoint
