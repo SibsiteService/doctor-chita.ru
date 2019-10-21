dce5a0fc1fad4052b64f674b710ca2fef95c5784
GOOSEBERRY <gooseberry.plusplus@gmail.com>
2019-10-21 21:07:27 +0300

    Yandex Metrica for doctor-chita

--- src/html.js
+++ src/html.js
@@ -27,20 +27,21 @@ export default function HTML(props) {
 		<script
   dangerouslySetInnerHTML={{
     __html: `
-               (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-   m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-
-   ym(55870498, "init", {
-        clickmap:true,
-        trackLinks:true,
-        accurateTrackBounce:true,
-        webvisor:true
-   });
+            (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+              m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+              (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+ 
+              ym(55704484, "init", {
+                clickmap:true,
+                trackLinks:true,
+                accurateTrackBounce:true,
+                webvisor:true
+              }
+            );
         `,
   }}
 />
-<noscript><div><img src="https://mc.yandex.ru/watch/55870498" style={{position:'absolute',left:-9999+'px' }} alt="" /></div></noscript>
+<noscript><div><img src="https://mc.yandex.ru/watch/55704484" style={{position:'absolute',left:-9999+'px' }} alt="" /></div></noscript>
       </body>
     </html>
   )
