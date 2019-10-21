649947064eccaad9b620978309534e6658ef5b74
GOOSEBERRY <gooseberry.plusplus@gmail.com>
2019-10-21 19:20:28 +0300

    test metrica

--- src/components/Header.js
+++ src/components/Header.js
@@ -16,7 +16,24 @@ const Header = (props) => (
         </p>
         <a href="http://n207269.yclients.com"  target="_blank" rel="noopener noreferrer" class="button fit">
                   ЗАПИСАТЬСЯ НА ПРИЕМ
-                </a>
+        </a>
+        <script
+            dangerouslySetInnerHTML={{
+            __html=`
+                (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                    m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+                    (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+     
+                    ym(55870498, "init", {
+                        clickmap:true,
+                        trackLinks:true,
+                        accurateTrackBounce:true,
+                        webvisor:true
+                });
+   	        `
+     }}
+     />   
+    <noscript><div><img src="https://mc.yandex.ru/watch/55870498" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
     </header>
     
 )
