e91d8e4db2a5f5c51c8a0126c98636c7048c39bc
GOOSEBERRY <gooseberry.plusplus@gmail.com>
2019-10-23 08:29:19 +0300

    Update: -перенос метрики в head

--- src/html.js
+++ src/html.js
@@ -12,6 +12,23 @@ export default function HTML(props) {
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
         {props.headComponents}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+              m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+              (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+ 
+              ym(55704484, "init", {
+                clickmap:true,
+                trackLinks:true,
+                accurateTrackBounce:true,
+                webvisor:true
+              });
+            `,
+            }}
+        />
+        <noscript><div><img src="https://mc.yandex.ru/watch/55704484" style={{position:'absolute',left:-9999+'px' }} alt="" /></div></noscript>
       </head>
       <body {...props.bodyAttributes}>
         {props.preBodyComponents}
@@ -24,24 +41,6 @@ export default function HTML(props) {
           dangerouslySetInnerHTML={{ __html: props.body }}
         />
         {props.postBodyComponents}
-		<script
-  dangerouslySetInnerHTML={{
-    __html: `
-            (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-              m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-              (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
- 
-              ym(55704484, "init", {
-                clickmap:true,
-                trackLinks:true,
-                accurateTrackBounce:true,
-                webvisor:true
-              }
-            );
-        `,
-  }}
-/>
-<noscript><div><img src="https://mc.yandex.ru/watch/55704484" style={{position:'absolute',left:-9999+'px' }} alt="" /></div></noscript>
       </body>
     </html>
   )
