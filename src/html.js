05a601e38d1f3105a2085eec5e6a1b8e14481dd7
SibsiteService <sibsite.development@gmail.com>
2021-04-05 22:57:24 +0200

    facebook pixel

--- src/html.js
+++ src/html.js
@@ -26,6 +26,23 @@ export default function HTML(props) {
         <script
           dangerouslySetInnerHTML={{
             __html: `
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '417673439442859');
+            fbq('track', 'PageView');
+            `,
+            }}
+        />
+        <noscript><img src="https://www.facebook.com/tr?id=417673439442859&ev=PageView&noscript=1" style={{position:'absolute',left:-9999+'px' }} alt="" /></noscript>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
             (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
               m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
               (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
@@ -40,6 +57,7 @@ export default function HTML(props) {
             }}
         />
         <noscript><div><img src="https://mc.yandex.ru/watch/55704484" style={{position:'absolute',left:-9999+'px' }} alt="" /></div></noscript>
+        
         {props.postBodyComponents}
       </body>
     </html>
