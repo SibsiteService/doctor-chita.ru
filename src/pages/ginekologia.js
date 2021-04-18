6cecf7e98232c46f50384e3d814fabf5f53f0533
SibsiteService <sibsite.development@gmail.com>
2021-04-18 15:57:34 +0200

    link defined

--- src/pages/ginekologia.js
+++ src/pages/ginekologia.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Helmet from 'react-helmet'
-
+import { Link } from 'gatsby'
 import Layout from '../components/layout'
 import Header from '../components/HeaderGeneric'
 import Nav from '../components/NavGeneric'
